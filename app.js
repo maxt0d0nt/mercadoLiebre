1dc0a2a7d7a236e3dab12d1e23bb7174fc4b9960
maxt0d0nt <maxbox21@hotmail.com>
2021-10-13 22:37:26 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const path = require("path")
+const app = express();
+
+app.use(express.static("public")),
+
+app.get("/", (req, res) => {
+    const archivo = path.join(__dirname, "./views/home.html");
+    res.sendFile(archivo);
+});
+
+app.get("/register", (req, res) => {
+    const archivo = path.join(__dirname, "/views/register.html");
+    res.sendFile(archivo);
+});
+
+app.listen(3030, () => console.log("servidor corriendo"));
